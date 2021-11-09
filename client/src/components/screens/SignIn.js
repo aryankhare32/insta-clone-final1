43578630ae0db3fdf5f9bc7f1ab0a89614d436f4
aryankhare32 <aryankhare32@gmail.com>
2021-11-09 15:17:44 +0530

    completed project fifth time

--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -61,9 +61,7 @@ const SignIn= ()=>{
             <h5>
                 <Link to="/signup">Don't have an account?</Link>
             </h5>
-//             <h6>
-//                 <Link to="/reset">Forgot Password?</Link>
-//             </h6>
+
             </div>
         </div>
     )
